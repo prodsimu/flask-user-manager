a25364cfcc86b8f11b82f203e3fb5af890627c8c
prodsimu <139299982+prodsimu@users.noreply.github.com>
2026-03-24 10:53:19 -0300

    feat: add UserService.verify_password

--- app/services.py
+++ app/services.py
@@ -3,8 +3,14 @@ import bcrypt
 
 class UserService:
 
+    # HASH
+
     @staticmethod
     def hash_password(password: str) -> str:
         salt = bcrypt.gensalt()
         hashed = bcrypt.hashpw(password.encode(), salt)
         return hashed.decode()
+
+    @staticmethod
+    def verify_password(password: str, hashed: str) -> bool:
+        return bcrypt.checkpw(password.encode(), hashed.encode())
