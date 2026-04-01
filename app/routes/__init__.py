e3d0e9dd1717e7d5d86497518a3eb88f3541ea8e
prodsimu <139299982+prodsimu@users.noreply.github.com>
2026-04-01 13:43:00 -0300

    feat(routes): export routes in __init__

--- app/routes/__init__.py
+++ app/routes/__init__.py
@@ -1,3 +1,4 @@
+from app.routes.member_routes import member_bp
 from app.routes.project_routes import project_bp
 from app.routes.task_routes import task_bp
 from app.routes.user_routes import user_bp
