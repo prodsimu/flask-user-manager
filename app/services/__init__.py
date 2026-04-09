2ac2361535b249cfdb8ea582f69ea7da218d8941
prodsimu <139299982+prodsimu@users.noreply.github.com>
2026-04-09 19:28:57 -0300

    refactor(init): clean up routes and services modules

--- app/services/__init__.py
+++
@@ -1,4 +0,0 @@
-from app.services.member_service import MemberService
-from app.services.project_service import ProjectService
-from app.services.task_service import TaskService
-from app.services.user_service import UserService
